dc91af9883a2a2c4ea9f84b1ff07fd6b2849a12e
Nate Tibbs <nates@Nates-MacBook-Pro.local>
2026-04-09 10:48:52 -0500

    Add gateway token to OpenClaw dashboard redirect

--- src/views/OpenClawView.jsx
+++ src/views/OpenClawView.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 
 function OpenClawView() {
   useEffect(() => {
-    window.location.href = 'http://localhost:18789'
+    window.location.href = 'http://localhost:18789/#token=4d8707be638d4f7cd95c2dfd09e563cdf8d8c864676c9def'
   }, [])
 
   return null
